71758927b9253576f6a369cdcb7ce628100486f0
jg764609 <jg764609@southeast.edu>
2022-07-10 14:31:08 -0500

    Object video notes.

---
+++ src/Objects.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Objects {
+
+	public static void main(String[] args) {
+		
+		int age = 34; //primative data type 
+		
+		String name = "Sally Mae"; 
+		System.out.println(name.length());
+		System.out.println(name.charAt(6));
+		
+		Scanner s = new Scanner(System.in); 
+		
+		
+		int[]numbers = new int[3];
+		System.out.println(numbers.length);
+		
+		
+	}
+
+}
