1afbb1da59018965ab797628597f899733c27119
jg764609 <jg764609@southeast.edu>
2022-07-10 12:54:56 -0500

    More Arrays video notes.

--- src/Arrays.java
+++ src/Arrays.java
@@ -29,6 +29,30 @@ public class Arrays {
 			System.out.println(student);
 		}
 		
-	
+		String [] products = new String[5]; 
+		
+		products [0]= "Carrots"; 
+		products [1]= "Pineapples"; 
+		products [2]= "Tomatoes"; 
+		products [3]= "Potatoes"; 
+		products [4]= "Cereal"; 
+		
+		for(String product : products) {
+			System.out.println("Product "+ product);
+		}
+		
+//		int[] multiplesOf3 = new int[10];
+//		
+//		for(int i = 1; i <= multiplesOf3.length; i++) {
+//			multiplesOf3[i -1]= i * 3;  
+//			System.out.println("number: " + multiplesOf3[i-1]); 
+//		}
+		
+		int[] multiplesOf5 = new int[10];
+		
+		for (int i = 0; i < multiplesOf5.length; i++) {
+			multiplesOf5[i]= (i+ 1) *5; 
+			System.out.println(multiplesOf5[i]);
+		}
 	}
 }
