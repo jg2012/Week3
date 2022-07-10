b363d2573503e7c9a6272796d2d1b0110ac40ee6
jg764609 <jg764609@southeast.edu>
2022-07-10 14:16:22 -0500

    Methods video notes complete.

--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,21 @@
 
 public class Methods {
 
+	public static void main(String[] args) {
+		
+	String 	firstName = "Bob";
+	String  lastName = "Ross";
+	String fullName = createFullName(firstName, lastName); 
+	String fullName2 = createFullName("Sam", lastName);
+	
+	System.out.println(fullName);
+	System.out.println(fullName2);
+	
+	}
+	
+	public static String createFullName(String x, String y) {
+		
+		return x + " " + y; 
+	}
+
 }
